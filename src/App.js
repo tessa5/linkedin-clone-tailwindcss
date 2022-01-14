e23c0558f0a514e749ed807e519fd80bbebd02b9
unknown <ktessa262@naver.com>
2022-01-14 16:35:50 +0300

    creating Login Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App overflow-y-scroll overflow-x-hidden bg-slate-100 box-border">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />}>
+          </Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = (props) =>{
+    return (
+        <div className="container p-0">
+        <nav className="max-w-screen-sm m-auto px-3 py-4 flex items-center relative justify-between flex-nowrap">
+            <a href='/' className="w-36 h-9">
+                <img src='/images/linkedinImg.png' alt=''/>
+            </a>
+        </nav>
+        </div>
+    )
+};
+
+export default Login
