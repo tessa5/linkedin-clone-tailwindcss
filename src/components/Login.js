e4032122639497bce0a1abe3f7437027d26e1dd8
unknown <ktessa262@naver.com>
2022-01-17 16:24:13 +0300

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,15 @@ const Login = (props) =>{
             <a href='/' className="w-36 h-9">
                 <img src='/images/linkedinImg.png' alt=''/>
             </a>
+            <div>
+                <a href='/' className="px-3 py-2 text-lg text-gray-700 hover:text-gray-800 font-medium mr-2 bg-slate-100 hover:bg-gray-300 rounded-md">Join now</a>
+                <a href='/' className="shadow rounded-full text-lg text-sky-800 transition duration-150 ease-in-out font-bold py-2.5 px-4 hover:bg-sky-200">Sign in</a>
+            </div>
         </nav>
+        <section className="flex content-start relative flex-wrap items-center min-h-min w-full max-w-screen-lg pb-32 pt-10 px-14 m-auto">
+            <h1 className="">Welcome to your professional community</h1>
+            <img src='/images/login-hero.svg' alt=''/>
+        </section>
         </div>
     )
 };
