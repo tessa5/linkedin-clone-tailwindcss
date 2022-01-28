b3d77a27c27e4b69c769005a6267c4596f314e9c
unknown <ktessa262@naver.com>
2022-01-28 16:20:03 +0300

    home-page header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className="container">
-            Header
+        <div className="container bg-white border-b-2  border-gray-800 z-50 left-0 top-0 fixed w-full px-4">
+            <div className="flex items-center my-auto">
+                <a href='/home'>
+                    <img className="ml-2" src='/images/home-logo.svg' alt='home-logo'/>
+                </a>
+            
+            <div className="">
+                <input
+                    type="text"
+                    placeholder="Search"
+                />
+            </div>
+            </div>
         </div>
     )
 }
